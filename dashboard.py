c5502606621b46903063491b12f4040f655279cf
Rashed Hossain <wprashed@icloud.com>
2025-04-23 21:33:53 +0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     issues = fetch_github_data()
     parsed_issues, parsed_prs = parse_github_data(issues)
-    return render_template("dashboard.html", issues=parsed_issues, prs=parsed_prs)
+    return render_template("dashboards.html", issues=parsed_issues, prs=parsed_prs)
 
 if __name__ == "__main__":
     app.run(debug=True)
